3a9a214d52e577bacb699fd3e20a351aca9038b9
Dimitar Vlahovski <dvlahovski@gmail.com>
2014-01-28 23:55:00 +0200

    Very bad version

--- hw.py
+++ hw.py
@@ -4,12 +4,12 @@ from subprocess import Popen, PIPE
 import sys
 from datetime import datetime
 
-scenarios = [("5 add 5", "10"), ("5 div 0", "error"), ("4 mod 2","0"), ("2 sub 1", "1"), ("12 mod 0", "error"), ("10 mul 10", "100"), 
-("asd asd asd as", "error"), ("12 qwe 3", "error"), ("add 5 5","error")]
+scenarios = [("98", "devetdesetiosem"), ("10", "deset"), ("23", "dvadesetitri"), ("45", "chetiridesetipet")]
 #TODO ^ This should be in a YAML/JSON file
 
 directory = "."
-homework_name = "homework_3"
+homework_name = "homework_5"
+program_name = "program7.c"
 #TODO ^ Those should be arguments (argparse)
 num_of_hw = 0
 
@@ -20,10 +20,10 @@ print datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 for root, dirs, files in os.walk(directory, topdown=False):
     if os.path.basename(root).lower() == homework_name.lower():
         print root
-        files = [ f for f in listdir(root) if (path.isfile(path.join(root,f)) and f.endswith(".c")) ]
+        files = [ f for f in listdir(root) if (path.isfile(path.join(root,f)) and f.endswith(".c")) and f == program_name ]
         
         if not files:
-            print "\tNo .c files in dir"
+            print "\tNo {0} file in dir".format(program_name)
             continue
         
         for current_file in files:
@@ -42,7 +42,8 @@ for root, dirs, files in os.walk(directory, topdown=False):
             for scenario in scenarios:
                 p = Popen([exec_path], stdout=PIPE, stderr=PIPE, stdin=PIPE)
                 std_out_data = p.communicate(scenario[0])
-                output = std_out_data[0].rstrip('\n').strip()
+                output = std_out_data[0].rstrip('\n').replace(' ', '').strip()
+                #output = output[:6]
     
                 sys.stdout.write("\t")
                 if output == scenario[1]:
