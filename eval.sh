4979f197ebb356cc392c865e78d4554a4bce2d30
Nikolay Bankin <niky@hacker.works>
2016-01-31 11:43:44 +0200

    Relative paths fix, new repo structure fix, evaling only the last 3 hws

--- eval.sh
+++ eval.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+CURR_DIR=$( pwd )
+
 TEACHER_TOOLS_DIR=$1
 PO_HW_DIR=$2
 CLASS_LETTER=$3
-HOMEWORK_NO=$4
-EVALUATOR=$1/elsys_tools/homework/evaluator.py
-TEST_CASE_DIR=$1/data/evaluator/scenarios
+LAST_HW_NO=$4
+EVALUATOR=$CURR_DIR/$TEACHER_TOOLS_DIR/elsys_tools/homework/evaluator.py
+TEST_CASE_DIR=$CURR_DIR/$TEACHER_TOOLS_DIR/data/evaluator/scenarios
 
-if [ -z "$TEACHER_TOOLS_DIR" -o -z "$PO_HW_DIR" -o -z "$CLASS_LETTER" -o -z "$HOMEWORK_NO" ]; then
+if [ -z "$TEACHER_TOOLS_DIR" -o -z "$PO_HW_DIR" -o -z "$CLASS_LETTER" -o -z "$LAST_HW_NO" ]; then
 	echo "Not all arguments supplied"
 	echo "Example usage of the program: "
 	echo "		./eval.sh /Volumes/Data/elsys/elsys-teachers-tools /Volumes/Data/elsys/po-homework V 05"
+    echo "NOTE: The number provided is the number of the last homework"
 	exit 1 
 fi
 
@@ -22,11 +25,19 @@ if [ "$CLASS_LETTER" == 'V' -o "$CLASS_LETTER" == 'G' ]; then
 	TEST_CASE_DIR=$TEST_CASE_DIR/V_G_class
 fi
 
-TEST_CASE_DIR=$TEST_CASE_DIR/$HOMEWORK_NO.toml
+TEST_CASE_DIR=$TEST_CASE_DIR/$LAST_HW_NO.toml
+
+currEvalHomework=$LAST_HW_NO
+cd $PO_HW_DIR/$CLASS_LETTER 
 
 for i in $( ls ); do
-	if [ -d "$i/$HOMEWORK_NO" ]; then
-        cd $PO_HW_DIR/2015-2016/$CLASS_LETTER/$i/$HOMEWORK_NO
-		python $EVALUATOR . $TEST_CASE_DIR -l DEBUG
-	fi
+    currEvalHomework=$LAST_HW_NO
+    for hw in `seq 0 2`; # This passes 3 times - 0, 1, 2
+    do
+        hwPath="$( printf "%02d/%02d" $((10#$i)) $((10#$currEvalHomework)))"
+        if [ -d "$hwPath" ]; then
+            python $EVALUATOR $hwPath $TEST_CASE_DIR -l DEBUG
+        fi
+        currEvalHomework=$((currEvalHomework-1))
+    done
 done
