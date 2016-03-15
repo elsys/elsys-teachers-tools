1be426474482bfe71f85fe188956f20c4ef95e41
Nikolay Bankin <niky@hacker.works>
2016-03-15 08:01:13 +0200

    eval.sh fixed and now working

--- eval.sh
+++ eval.sh
@@ -6,7 +6,7 @@ TEACHER_TOOLS_DIR=$1
 PO_HW_DIR=$2
 LAST_HW_NO=$3
 EVALUATOR=$CURR_DIR/$TEACHER_TOOLS_DIR/elsys_tools/homework/evaluator.py
-TEST_CASE_DIR=$CURR_DIR/$TEACHER_TOOLS_DIR/data/evaluator/scenarios
+BASE_TEST_CASE_DIR=$CURR_DIR/$TEACHER_TOOLS_DIR/data/evaluator/scenarios
 
 if [ -z "$TEACHER_TOOLS_DIR" -o -z "$PO_HW_DIR" -o -z "$LAST_HW_NO" ]; then
 	echo "Not all arguments supplied"
@@ -18,12 +18,13 @@ fi
 
 for letter in "A" "B" "V" "G"
 do
+    echo $letter
     if [ "$letter" == "A" -o "$letter" == "B" ]; then
-    	TEST_CASE_DIR=$TEST_CASE_DIR/A_B_class
+    	TEST_CASE_DIR=$BASE_TEST_CASE_DIR/A_B_class
     fi
 
     if [ "$letter" == 'V' -o "$letter" == 'G' ]; then
-	    TEST_CASE_DIR=$TEST_CASE_DIR/V_G_class
+	    TEST_CASE_DIR=$BASE_TEST_CASE_DIR/V_G_class
     fi
 
     TEST_CASE_DIR=$TEST_CASE_DIR/$LAST_HW_NO.toml
