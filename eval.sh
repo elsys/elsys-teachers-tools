02a5bdb5cbaf0bbdc1a4eea4b4f67d419af81664
Nikolay Bankin <niky@hacker.works>
2016-03-15 11:39:31 +0200

    eval.sh evaling of multiple homeworks. Flag for evaling single homework

--- eval.sh
+++ eval.sh
@@ -7,6 +7,13 @@ PO_HW_DIR=$2
 LAST_HW_NO=$3
 EVALUATOR=$CURR_DIR/$TEACHER_TOOLS_DIR/elsys_tools/homework/evaluator.py
 BASE_TEST_CASE_DIR=$CURR_DIR/$TEACHER_TOOLS_DIR/data/evaluator/scenarios
+ONLY_ONE_HW=$4
+
+if [ -z "$ONLY_ONE_HW"]; then
+    ONLY_ONE_HW=0
+fi
+
+echo $ONLY_ONE_HW
 
 if [ -z "$TEACHER_TOOLS_DIR" -o -z "$PO_HW_DIR" -o -z "$LAST_HW_NO" ]; then
 	echo "Not all arguments supplied"
@@ -27,24 +34,23 @@ do
 	    TEST_CASE_DIR=$BASE_TEST_CASE_DIR/V_G_class
     fi
 
-    TEST_CASE_DIR=$TEST_CASE_DIR/$LAST_HW_NO.toml
-
     currEvalHomework=$LAST_HW_NO
     cd $CURR_DIR/$PO_HW_DIR/$letter
     
     for hw in `seq 0 2`; # This passes 3 times - 0, 1, 2
     do
+        TOML_DIR=$TEST_CASE_DIR"$( printf "/%02d.toml" $((10#$currEvalHomework)))"
 
         for i in `seq 1 29`;
         do
             hwPath="$( printf "%02d/%02d" $((10#$currEvalHomework)) $((10#$i)))" 
             #echo $hwPath
             if [ -d "$hwPath" ]; then
-                python $EVALUATOR $hwPath $TEST_CASE_DIR -l DEBUG
+                python $EVALUATOR $hwPath $TOML_DIR -l DEBUG
             fi
         done
         currEvalHomework=$((currEvalHomework-1))
-        if [ $currEvalHomework -lt 1 ]; then
+        if [ $currEvalHomework -lt 1 -o $ONLY_ONE_HW -eq 1 ]; then
             break
         fi
     done
